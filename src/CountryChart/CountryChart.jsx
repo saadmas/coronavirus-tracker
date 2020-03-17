b83afc7bdf15a02ba6b7f5420dbd18b201edcee3
Saad <saadmasood96@hotmail.com>
2020-03-16 23:39:14 -0400

    configure chart tooltip

--- src/CountryChart/CountryChart.jsx
+++ src/CountryChart/CountryChart.jsx
@@ -40,23 +40,40 @@ const CountryChart = ({ virusData }) => {
     }
   };
 
-  const getMaxConfirmed = () => {
-    console.log(chartData[chartData.length - 1]['confirmed'])
-    return chartData[chartData.length - 1]['confirmed'];
+  const onTooltip = (e) => {
+    if (e.payload.length < 2) {
+      return;
+    }
+    const payload = e.payload[1]['payload'];
+    const date = payload['date'];
+    const confirmed = payload['confirmed'];
+    const deaths = payload['deaths'];
+    return (
+      <div className="tooltipStats">
+        <ul className="tooltipStatsList">
+          <li>Date: {date}</li>
+          <li>#Confirmed: {confirmed}</li>
+          <li>#Deaths: {deaths}</li>
+        </ul>
+      </div>
+    );
   };
 
   const render = () => {
     if (!(!!chartData) || chartData.length === 0) {
       return null;
     }
+
+    const maxConfirmed = chartData[chartData.length - 1]['Confirmed'];
+
     return (
       <div className="countryChart">
         <LineChart width={1000} height={500} data={chartData}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="date" />
-          <YAxis domain={[0, getMaxConfirmed()]} />
-          <Tooltip />
+          <YAxis domain={[0, maxConfirmed]} />
+          <Tooltip content={onTooltip} />
           <Legend />
           <Line type="monotone" dataKey="confirmed" stroke="#8884d8" />
           <Line type="monotone" dataKey="deaths" stroke="#82ca9d" />
