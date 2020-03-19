71e06b3b81f91a25bb5c185ba43c3624d170626a
Saad <saadmasood96@hotmail.com>
2020-03-19 00:32:29 -0400

    handle case where country has no reported cases

--- src/Pages/CountryChartPage/CountryChartPage.jsx
+++ src/Pages/CountryChartPage/CountryChartPage.jsx
@@ -5,22 +5,26 @@ import CountryChart from '../../Components/CountryChart/CountryChart';
 
 const CountryChartPage = ({ virusData }) => {
   const [selectedCountry, setSelectedCountry] = React.useState('');
+  const [countryHasNoCases, setCountryHasNoCases] = React.useState('');
 
-  const getIndexOfFirstConfirmed = (data) => {
+  const getIndexOfFirstConfirmed = (data, countryName) => {
     let i = 0;
     let confirmed = data[i]['Confirmed'];
     while (confirmed < 1 && i < data.length) {
       i++;
       confirmed = data[i]['Confirmed'];
     }
-    return i;
-    /// handle no reported cases
+    if (confirmed < 1) {
+      setCountryHasNoCases(countryName);
+    } else {
+      return i;
+    }
   };
 
   const getChartData = (countryName) => {
     let countryData = virusData.filter(c => c['CountryName'] === countryName);
 
-    const indexOfFirstConfirmed = getIndexOfFirstConfirmed(countryData);
+    const indexOfFirstConfirmed = getIndexOfFirstConfirmed(countryData, countryName);
     countryData = countryData.slice(indexOfFirstConfirmed);
 
     const chartData = countryData.map(d => ({
@@ -32,6 +36,12 @@ const CountryChartPage = ({ virusData }) => {
   };
 
   const render = () => {
+    if (countryHasNoCases) {
+      return (
+        <h2>{countryHasNoCases} has no reported cases at this time</h2>
+      );
+    };
+
     if (selectedCountry) {
       const chartData = getChartData(selectedCountry);
       return (
