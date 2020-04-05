3c1726749eed08766ab5b402decfef413e3f23aa
Saad <saadmasood96@hotmail.com>
2020-04-05 17:01:03 -0400

    dark theme and responsive design for Stats Table

--- src/Components/StatsTable/StatsTable.jsx
+++ src/Components/StatsTable/StatsTable.jsx
@@ -1,30 +1,77 @@
 import React from 'react';
 import MUIDataTable from 'mui-datatables';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
+import { getNumberWithCommas, getDecimalCount } from '../../utils';
 
 import './StatsTable.css'
 
 const StatsTable = ({ tableData, history }) => {
+
+  const getMuiTheme = () => createMuiTheme({
+    overrides: {
+      'MUIDataTableBodyCell': {
+        root: {
+          backgroundColor: 'black',
+          color: 'white'
+        }
+      },
+      'MuiToolbar': {
+        root: {
+          backgroundColor: 'black',
+          color: 'white'
+        }
+      },
+      'MuiSvgIcon': {
+        root: {
+          color: 'white'
+        }
+      },
+      'MuiTableCell': {
+        footer: {
+          backgroundColor: 'black'
+        }
+      }
+    }
+  });
+
   /// US toggle
   const columns = [
     {
       name: 'countryName',
-      label: 'Country Name'
+      label: 'Country Name',
+      options: {
+        hint: 'Click on a country name to chart that country\'s data'
+      }
     },
     {
       name: 'confirmed',
       label: 'Reported Cases',
       options: {
-        sortDirection: 'desc'
+        sortDirection: 'desc',
+        customBodyRender: (numberOfConfirmed) => getNumberWithCommas(numberOfConfirmed)
       }
     },
     {
       name: 'deaths',
-      label: 'Reported Deaths'
+      label: 'Reported Deaths',
+      options: {
+        customBodyRender: (numberOfDeaths) => getNumberWithCommas(numberOfDeaths)
+      }
     },
     {
       name: 'mortalityRate',
-      label: 'Mortality Rate'
+      label: 'Mortality Rate',
+      options: {
+        customBodyRender: (mortalityRate) => {
+          let formattedMortalityRate = mortalityRate;
+          if (getDecimalCount(mortalityRate) > 0) {
+            formattedMortalityRate = mortalityRate.toFixed(1);
+          }
+          formattedMortalityRate += "%"
+          return formattedMortalityRate;
+        }
+      }
     },
   ];
 
@@ -40,22 +87,21 @@ const StatsTable = ({ tableData, history }) => {
   const options = {
     selectableRows: 'none',
     rowsPerPage: 50,
-    searchOpen: true,
+    responsive: 'scrollFullHeight',
     print: false,
     download: false,
+    viewColumns: false,
     onCellClick
   };
 
   const transformTableData = () => {
     const transformedData = tableData.map(c => {
-      /// number with commas, mortality rate decimal place
-      /// handle / 0 Mortality Rate
       /// US toggle
       return {
         countryName: c['CountryName'],
         confirmed: c['Confirmed'],
         deaths: c['Deaths'],
-        mortalityRate: c['Confirmed'] / c['Deaths']
+        mortalityRate: (c['Deaths'] / c['Confirmed']) * 100
       };
     });
     return transformedData;
@@ -65,12 +111,14 @@ const StatsTable = ({ tableData, history }) => {
     return (
       <div className="statsTable">
         {/* /// US toggle */}
-        <h3 className="statsTableHeader"> Country Statistics Table</h3>
-        <MUIDataTable
-          data={transformTableData()}
-          columns={columns}
-          options={options}
-        />
+        <h3 className="statsTableHeader"> Country Statistics</h3>
+        <MuiThemeProvider theme={getMuiTheme()}>
+          <MUIDataTable
+            data={transformTableData()}
+            columns={columns}
+            options={options}
+          />
+        </MuiThemeProvider>
       </div>
     );
   };
