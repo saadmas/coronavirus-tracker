81d6b07362117e3a8a1df4d92b4b918d6e401ce7
Saad <saadmasood96@hotmail.com>
2020-04-10 18:14:37 -0400

    toggle between country and US State on stats page

--- src/Pages/StatsPage/StatsPage.jsx
+++ src/Pages/StatsPage/StatsPage.jsx
@@ -4,22 +4,44 @@ import { getLatestData, getLatestDataForUnitedStates } from '../../utils';
 
 
 const StatsPage = ({ virusData }) => {
-  ///
   const [isCountryOrUSState, setIsCountryOrUSState] = React.useState('Country');
+  const [countryTableData, setCountryTableData] = React.useState([]);
+  const [USStateTableData, setUSStateTableData] = React.useState([]);
 
-  /// put into state
+  React.useEffect(() => {
+    getCountryOnlyData();
+    getUSOnlyData();
+  }, [virusData]);
+
   const getCountryOnlyData = () => {
     const latestData = getLatestData(virusData);
     const countryOnlyData = latestData.filter(x => !x['RegionCode'] && !x['RegionName']);
-    return countryOnlyData;
+    setCountryTableData(countryOnlyData);
+  };
+
+  const getUSOnlyData = () => {
+    let USData = getLatestDataForUnitedStates(virusData);
+
+    USData = USData.map(s => ({
+      ...s,
+      Deaths: s['Deaths'] || 0
+    }));
+
+    setUSStateTableData(USData);
+  };
+
+  const getTableData = () => {
+    if (isCountryOrUSState === 'Country') {
+      return countryTableData;
+    }
+    return USStateTableData;
   };
 
   return (
     <StatsTable
       isCountryOrUSState={isCountryOrUSState}
       setIsCountryOrUSState={setIsCountryOrUSState}
-      tableData={getCountryOnlyData()}
-      isCountryOrUSState="Country"
+      tableData={getTableData()}
     />
   );
 };
