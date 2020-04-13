8df0fb5c3041e4d81b0825cc505d98cbcc05d8c1
Saad <saadmasood96@hotmail.com>
2020-04-12 23:52:45 -0400

    error page include Stats

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -7,9 +7,13 @@ const ErrorPage = () => {
         Uh oh... Something went wrong.
         <br />
         <br />
-        You can click on "Map" to see a world map or U.S. state map of COVID-19 cases and deaths,
+        You can click on:
+        <br />
+        "Map" to see a world map or U.S. state map of COVID-19 cases and deaths,
         <br />
         "Chart" to see a timeline of data for a specific country or U.S. state,
+        <br />
+        "Stats" to see data in table format for countries or U.S. states collectively,
         <br />
         or "About" to learn more about COVID-19 and the data behind this website
       </h2>
