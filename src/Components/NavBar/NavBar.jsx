641f01ba918cca478f71b60330dcc53a327cab66
Saad <saadmasood96@hotmail.com>
2020-05-10 18:18:38 -0400

    add Trends to NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import PublicIcon from '@material-ui/icons/Public';
 import InfoIcon from '@material-ui/icons/Info';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import TableChartIcon from '@material-ui/icons/TableChart';
+import TrendingDownIcon from '@material-ui/icons/TrendingDown';
 
 import './NavBar.css';
 
@@ -10,21 +11,27 @@ const NavBar = () => {
   return (
     <div className="navBar">
       <div>
-        <a href='/' className="navLink mapLink">
+        <a href='/' className="navLink">
           <span className="navLinkText">Map </span>
           <PublicIcon className="navIcon mapIcon" />
         </a>
       </div>
       <div>
-        <a href='/stats' className="navLink statsLink">
+        <a href='/stats' className="navLink">
           <span className="navLinkText">Stats </span>
           <TableChartIcon className="navIcon statsIcon" />
         </a>
       </div>
       <div>
-        <a href='/chart' className="navLink chartLink">
+        <a href='/chart' className="navLink">
           <span className="navLinkText">Chart </span>
           <AssessmentIcon className="navIcon chartIcon" />
+        </a>
+      </div>
+      <div>
+        <a href='/trends' className="navLink">
+          <span className="navLinkText">Trends </span>
+          <TrendingDownIcon className="navIcon trendsIcon" />
         </a>
       </div>
       <div>
