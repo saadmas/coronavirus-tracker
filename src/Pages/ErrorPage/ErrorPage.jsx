002934cf4833968717149dfcbc34bc110ead1147
Saad <saadmasood96@hotmail.com>
2020-05-25 17:27:59 -0400

    404 page info for trends, CSS fixes

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
+import './ErrorPage.css';
+
 const ErrorPage = () => {
   return (
     <div>
       <h2>
         Uh oh... Something went wrong.
-        <br />
-        <br />
-        You can click on:
-        <br />
-        "Map" to see a world map or U.S. state map of COVID-19 cases and deaths,
-        <br />
-        "Chart" to see a timeline of data for a specific country or U.S. state,
-        <br />
-        "Stats" to see data in table format for countries or U.S. states collectively,
-        <br />
-        or "About" to learn more about COVID-19 and the data behind this website
+        <div className="siteMapSection">
+          <p>
+            <span className="clickOptions">You can click on:</span>
+          </p>
+          <ul className="siteMapList">
+            <li>"Map" to see a world map or U.S. state map of COVID-19 cases and deaths</li>
+            <li>"Stats" to see data in table format for countries or U.S. states collectively</li>
+            <li>"Chart" to see a timeline of data for a specific country or U.S. state</li>
+            <li>"Trends" to chart movement trends over time by geography, across different categories of place, via data from Google</li>
+            <li>"About" to learn more about COVID-19 and the data behind this website</li>
+          </ul>
+        </div>
       </h2>
     </div>
   )
