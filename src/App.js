cf744418d0f440c339277efe3fbd73d5ff010906
Saad <saadmasood96@hotmail.com>
2020-05-29 23:40:28 -0400

    add loading spinner to app init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import ChartPage from './Pages/ChartPage/ChartPage';
 import MapPage from './Pages/MapPage/MapPage';
 import StatsPage from './Pages/StatsPage/StatsPage';
@@ -17,10 +18,12 @@ const App = () => {
   const [virusData, setVirusData] = React.useState([]);
   const [trendsData, setTrendsData] = React.useState([]);
   const [isDataFetchError, setIsDataFetchError] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     getCoronavirusData();
     getMobilityData();
+    setIsLoading(false);
   }, []);
 
   const getCoronavirusData = async () => {
@@ -58,44 +61,60 @@ const App = () => {
     );
   }
 
+  const getRoutes = () => {
+    if (virusData && virusData.length) {
+      return (
+        <Switch>
+          <Route
+            exact
+            path="/chart/:chartType?/:regionName?"
+            render={(props) => <ChartPage virusData={virusData} {...props} />}
+          />
+          <Route
+            exact
+            path="/trends/:chartType?/:regionName?"
+            render={(props) => <TrendsPage virusData={virusData} trendsData={trendsData} {...props} />}
+          />
+          <Route
+            exact
+            path="/stats"
+            render={(props) => <StatsPage virusData={virusData} {...props} />}
+          />
+          <Route
+            exact
+            path="/about"
+            render={(props) => <AboutPage />}
+          />
+          <Route
+            exact
+            path="/"
+            render={(props) => <MapPage virusData={virusData} {...props} />}
+          />
+          <Route component={ErrorPage} />
+        </Switch>
+      );
+    }
+  }
+
+  const getAppContent = () => {
+    if (isDataFetchError) {
+      return <h2>Uh oh... Error fetching COVID-19 data. <br />Please refresh the page to try again.</h2>
+    }
+
+    if (isLoading) {
+      return <CircularProgress color="secondary" />
+    }
+
+    return getRoutes();
+  }
+
   return (
     <div className="App">
       <CssBaseline />
       <Container>
         <h1 className="siteTitle">COVID-19 Daily Tracker</h1>
         <NavBar />
-        {
-          virusData && virusData.length > 0 && (
-            <Switch>
-              <Route
-                exact
-                path="/chart/:chartType?/:regionName?"
-                render={(props) => <ChartPage virusData={virusData} {...props} />}
-              />
-              <Route
-                exact
-                path="/trends/:chartType?/:regionName?"
-                render={(props) => <TrendsPage virusData={virusData} trendsData={trendsData} {...props} />}
-              />
-              <Route
-                exact
-                path="/stats"
-                render={(props) => <StatsPage virusData={virusData} {...props} />}
-              />
-              <Route
-                exact
-                path="/about"
-                render={(props) => <AboutPage />}
-              />
-              <Route
-                exact
-                path="/"
-                render={(props) => <MapPage virusData={virusData} {...props} />}
-              />
-              <Route component={ErrorPage} />
-            </Switch>
-          )
-        }
+        {getAppContent()}
       </Container>
     </div>
   );
