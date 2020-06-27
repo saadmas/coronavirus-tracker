04873837636aedc77f910d559234e1085227e039
Saad <saadmasood96@hotmail.com>
2020-06-27 17:30:05 -0400

    use Paper for TrendsPage lists

--- src/Pages/TrendsPage/TrendsPage.jsx
+++ src/Pages/TrendsPage/TrendsPage.jsx
@@ -4,6 +4,7 @@ import RegionSelect, { USPrefix } from '../../Components/RegionSelect/RegionSele
 import TrendsChart from '../../Components/TrendsChart/TrendsChart';
 import { getCountryNames, getUSStateNames, getUSStateCode } from './trendsPage.utils';
 import { parseChartSettingsFromParams } from '../../utils';
+import Paper from '@material-ui/core/Paper';
 
 import './TrendsPage.css';
 
@@ -94,8 +95,8 @@ const TrendsPage = ({ trendsData, match, history }) => {
       </div>
       <div className="trendsInfo">
         <p>
-          <a href="https://www.google.com/covid19/mobility/" className="aLink">Community Mobility Reports</a> 
-          &nbsp;by <b> Google </b> 
+          <a href="https://www.google.com/covid19/mobility/" className="aLink">Community Mobility Reports</a>
+          &nbsp;by <b> Google </b>
           aim to provide insights into what has changed in response to policies aimed at combating COVID-19.
         </p>
         <p>
@@ -103,45 +104,63 @@ const TrendsPage = ({ trendsData, match, history }) => {
           during the 5-week period Jan 3 – Feb 6, 2020.
         </p>
         <p>
-          The reports chart movement trends over time by geography, across different categories of places such as:
+          The reports chart movement trends over time by geography, across different categories of places listed below.
         </p>
-        <h3>Grocery &amp; pharmacy</h3>
-        <ul className="trendsInfoList">
-          <li>Grocery markets</li>
-          <li>Food warehouses</li>
-          <li>Farmers markets</li>
-          <li>Specialty food shops</li>
-          <li>Drug stores</li>
-          <li>Pharmacies</li>
-        </ul>
-        <h3>Parks</h3>
-        <ul>
-          <li>Local parks</li>
-          <li>National parks</li>
-          <li>Public beaches</li>
-          <li>Marinas</li>
-          <li>Dog parks</li>
-          <li>Plazas</li>
-          <li>Public gardens</li>
-        </ul>
-        <h3>Transit stations</h3>
-        <ul>
-          <li>Subway stations</li>
-          <li>Bus stations</li>
-          <li>Train stations</li>
-        </ul>
-        <h3>Retail &amp; recreation</h3>
-        <ul>
-          <li>Restaurants</li>
-          <li>Cafes</li>
-          <li>Shopping centers</li>
-          <li>Theme parks</li>
-          <li>Museums</li>
-          <li>Libaries</li>
-          <li>Movie theaters</li>
-        </ul>
-        <h3>Residential</h3><br />
-        <h3>Workplaces</h3><br />
+        <section className="PaperContainer">
+          <Paper className="Paper GroceryPaper" elevation={24}>
+            <h3>Grocery &amp; pharmacy</h3>
+            <hr />
+            <ul className="trendsInfoList">
+              <li>Grocery markets</li>
+              <li>Food warehouses</li>
+              <li>Farmers markets</li>
+              <li>Specialty food shops</li>
+              <li>Drug stores</li>
+              <li>Pharmacies</li>
+            </ul>
+          </Paper>
+          <Paper className="Paper ParksPaper" elevation={24}>
+            <h3>Parks</h3>
+            <hr />
+            <ul>
+              <li>Local parks</li>
+              <li>National parks</li>
+              <li>Public beaches</li>
+              <li>Marinas</li>
+              <li>Dog parks</li>
+              <li>Plazas</li>
+              <li>Public gardens</li>
+            </ul>
+          </Paper>
+          <Paper className="Paper TransitPaper" elevation={24}>
+            <h3>Transit stations</h3>
+            <hr />
+            <ul>
+              <li>Subway stations</li>
+              <li>Bus stations</li>
+              <li>Train stations</li>
+            </ul>
+          </Paper>
+          <Paper className="Paper RetailPaper" elevation={24}>
+            <h3>Retail &amp; recreation</h3>
+            <hr />
+            <ul>
+              <li>Restaurants</li>
+              <li>Cafes</li>
+              <li>Shopping centers</li>
+              <li>Theme parks</li>
+              <li>Museums</li>
+              <li>Libaries</li>
+              <li>Movie theaters</li>
+            </ul>
+          </Paper>
+          <Paper className="Paper ResidentialPaper" elevation={24}>
+            <h3>Residential</h3><br />
+          </Paper>
+          <Paper className="Paper WorkplacePaper" elevation={24}>
+            <h3>Workplaces</h3><br />
+          </Paper>
+        </section>
       </div>
     </div>
   );
